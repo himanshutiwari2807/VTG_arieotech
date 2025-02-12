e33c6299b294f345a7a7d8c8aac8ee878cf6e0c5
Himanshu Tiwari <himanshu.tiwari@arieotech.com>
2025-02-12 20:11:08 +0530

    Added the video transcribe service and json output

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,10 @@
 class VideosController < ApplicationController
 
   def create
-    url = params[:url]
-    if url.present?
-      video_id = url.split('=')[-1]
+    video_url = video_params[:url]
+
+    if video_url.present?
+      video_id = video_url.split('=')[-1]
       # Check if the video URL exists, create it if not
       video = Video.find_or_create_by(url: video_id)
 
@@ -11,22 +12,37 @@ class VideosController < ApplicationController
         # Run the Python script if the URL is saved successfully
         folder_path = Rails.root.join('public', 'video', video_id)
 
-        unless Dir.exist?(folder_path)
-          # Run the Python script to generate the files
-          system("python3 #{Rails.root}/scripts/generate_subtitles.py #{url} #{folder_path}")
-        end
+        if Dir.exist?(folder_path)
+          json_file_path = File.join(folder_path, 'audio.json')
+        else
+          transcript_service = TranscriptService.new(folder_path, video_url)
+          transcript_service.create_audio_and_transcribe
 
-        render json: { message: 'Video URL saved and processing started.' }, status: :created
+          tsv_file_path = File.join(folder_path, 'audio.tsv')
+          if File.exist?(tsv_file_path)
+            json_converter_service = JsonConverterService.new(tsv_file_path)
+            output = json_converter_service.parse_tsv
+
+            json_file_path = tsv_file_path.sub(/\.tsv$/, '.json')
+
+            File.write(json_file_path, JSON.pretty_generate(output))
+            puts "JSON file saved at: #{json_file_path}"
+
+          else
+            render json: { error: 'Error processing Video URL' }, status: :unprocessable_entity
+          end
+        end
+        send_file json_file_path, type: 'application/json', disposition: 'attachment'
       else
         render json: { error: 'Video URL already exists.' }, status: :unprocessable_entity
       end
     else
-
+      render json: { error: 'Video URL is required.' }, status: :unprocessable_entity
     end
   end
 
   def show
-    video_url = params[:url]
+    video_url = video_params[:url]
     if video_url
       video_id = URI(video_url).query.split("&").find { |param| param.start_with?("v=") }&.split("=")&.last
       video = Video.find_by(url: video_id)
@@ -47,6 +63,12 @@ class VideosController < ApplicationController
     else
       render json: { error: 'URL not found.' }, status: :not_found
     end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:url)
   end
 end
 
